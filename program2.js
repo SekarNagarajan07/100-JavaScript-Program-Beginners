2bc6c82d56f1be9f2c059d62f33f087d77765baf
sekar N <sekarnagarajan1234@gmail.com>
2024-03-06 22:03:15 +0530

    js

---
+++ program2.js
@@ -0,0 +1,9 @@
+//Create variables for your name, age, and favorite color, and print them to the console.
+
+var name = "sekar";
+var age = 21;
+var favoriteColor = "blue";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Color: " + favoriteColor);
